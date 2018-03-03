6cbcbd9087ef26f16ee1a436da0b542ed5dd1119
Brandon <bsteffe420@gmail.com>
2018-03-02 20:19:31 -0600

    added command to install a list of snaps

--- myinstall.sh
+++ myinstall.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#This script is for a quick way to install all my default apps
+# This script is for a quick way to install all my default apps
+# 
 
 # This is the variable defining the package manager in use. Change depending on your package manager.
 MANAGER="eopkg"
 
-# Variable for the list of packages
+# Variable for the list of packages you wish to install
 PACKAGES="vim neofetch htop ncdu nmap tmux zsh git"
 
+# Variable for the list of snap packages you wish to install
+SNAPS="discord spotify mailspring tusk"
 
-sudo $MANAGER install -y $PACKAGES
+ sudo $MANAGER install -y $PACKAGES
+
+# Enable if you have snaps support
+ snap install $SNAPS
 
